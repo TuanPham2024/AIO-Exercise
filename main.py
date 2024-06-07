9c1b8483bb0ef55c220245967e12ef50bb1b6223
AIO-326-TuanPham <tuanpham.openai@gmail.com>
2024-06-07 12:51:59 +0700

    feature/gitday2-Add substract|multiply|exponent

--- main.py
+++ main.py
@@ -2,6 +2,15 @@
 def sum(a, b):
     return a + b
 
+def substract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a*b
+
+def exponent(a, n):
+    return a**n
+
 #kiểm tra xem module hiện tại (__name__) == "__main__" hay không, 
 # tức đang chạy như một script độc lập chứ không phải được import từ một module khác
 # Kỹ thuật dùng để ngăn chặn code chạy khi file được import như một module
